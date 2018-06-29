0acf413a3b3b7087d5b0850b166ce84b23b17eaf
Elizabeth Chan <lmchan@brynmawr.edu>
2018-06-29 19:26:59 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -18,4 +18,4 @@ nights = gets.chomp
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "#{stay} is a beautiful place to eat #{eats} and drink for #{nights} nights!"
+puts "#{stay} is a beautiful place to eat #{eat} and drink for #{nights} nights!"
